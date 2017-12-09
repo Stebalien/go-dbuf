a8db11e4d876365afdf8e89be40cb4b5825fdad3
Steven Allen <steven@stebalien.com>
2017-12-08 16:14:07 -0800

    really nice benchmarks
    closes #4

--- dbuf_test.go
+++ dbuf_test.go
@@ -3,17 +3,25 @@ package dbuf
 import (
 	"bufio"
 	"bytes"
-	"crypto/rand"
+	"encoding/binary"
+	"fmt"
 	"io"
 	"io/ioutil"
+	"math/rand"
 	"net"
 	"testing"
 )
 
-const size = 256
 const bufsize = 1024 * 256
 
-func tcpPipe(t testing.TB) (net.Conn, net.Conn) {
+type pipeFunc func(testing.TB) (io.ReadCloser, io.WriteCloser)
+type writeBufferFunc func(w io.WriteCloser) io.WriteCloser
+type chopperFunc func(r uint32) int
+type chopperMaker func() chopperFunc
+
+// Pipes
+
+func tcpPipe(t testing.TB) (io.ReadCloser, io.WriteCloser) {
 	list, err := net.Listen("tcp", "127.0.0.1:0")
 	if err != nil {
 		t.Fatal(err)
@@ -32,67 +40,11 @@ func tcpPipe(t testing.TB) (net.Conn, net.Conn) {
 	return con1, con2
 }
 
-var pipe = tcpPipe
+func ioPipe(testing.TB) (io.ReadCloser, io.WriteCloser) { return io.Pipe() }
 
-//var pipe = func(testing.TB) (io.ReadCloser, io.WriteCloser) { return io.Pipe() }
+// Buffers
 
-func doTest(t testing.TB, data []byte, out io.WriteCloser, in io.ReadCloser) {
-	go func(data []byte) {
-		defer out.Close()
-		for len(data) > 0 {
-			n, err := out.Write(data[:size])
-			data = data[n:]
-			if err != nil {
-				t.Fatal(err)
-			}
-		}
-	}(data[:])
-
-	dataout, err := ioutil.ReadAll(in)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if !bytes.Equal(dataout, data[:]) {
-		t.Fatal("got bad data")
-	}
-}
-
-func BenchmarkDBuf(t *testing.B) {
-	var data [bufsize]byte
-
-	if _, err := rand.Read(data[:]); err != nil {
-		t.Fatal(err)
-	}
-
-	t.StopTimer()
-	t.ResetTimer()
-
-	for i := 0; i < t.N; i++ {
-		in, out := pipe(t)
-		bufout := NewWriter(out)
-		t.StartTimer()
-		doTest(t, data[:], bufout, in)
-		t.StopTimer()
-	}
-}
-
-func BenchmarkNormal(t *testing.B) {
-	var data [bufsize]byte
-
-	if _, err := rand.Read(data[:]); err != nil {
-		t.Fatal(err)
-	}
-
-	t.ResetTimer()
-	t.StopTimer()
-
-	for i := 0; i < t.N; i++ {
-		in, out := pipe(t)
-		t.StartTimer()
-		doTest(t, data[:], out, in)
-		t.StopTimer()
-	}
-}
+func writeBufferNone(w io.WriteCloser) io.WriteCloser { return w }
 
 type BufWriteCloser struct {
 	bw    bufio.Writer
@@ -112,21 +64,145 @@ func (b *BufWriteCloser) Write(buf []byte) (int, error) {
 	return b.bw.Write(buf)
 }
 
-func BenchmarkBuf(t *testing.B) {
+func writeBufferBufio(w io.WriteCloser) io.WriteCloser {
+	return &BufWriteCloser{bw: *bufio.NewWriter(w), inner: w}
+}
+
+func writeBufferDBuf(w io.WriteCloser) io.WriteCloser {
+	return NewWriter(w)
+}
+
+// Choppers
+
+func randomSmallChopper() chopperFunc {
+	return func(r uint32) int {
+		size := 2 << (r % 8)
+		size += int(r) % size
+		return size
+	}
+}
+
+func randomLargeChopper() chopperFunc {
+	return func(r uint32) int {
+		size := 2 << (r % 12)
+		size += int(r) % size
+		return size
+	}
+}
+
+func alternatingSmallChopper() chopperFunc {
+	small := false
+	return func(r uint32) int {
+		small = !small
+		if small {
+			return 10
+		}
+		return int(r % 512)
+	}
+}
+
+func alternatingMediumChopper() chopperFunc {
+	small := false
+	return func(r uint32) int {
+		small = !small
+		if small {
+			return 10
+		}
+		return int(r % 2929)
+	}
+}
+func alternatingLargeChopper() chopperFunc {
+	small := false
+	return func(r uint32) int {
+		small = !small
+		if small {
+			return 10
+		}
+		return int(r % 8000)
+	}
+}
+
+// Test function
+func doTest(t testing.TB, data []byte, chopper chopperFunc, out io.WriteCloser, in io.ReadCloser) {
+	go func(data []byte) {
+		defer out.Close()
+		for len(data) > 0 {
+			write := data
+			if len(data) > 16 {
+				size := chopper(binary.BigEndian.Uint32(data))
+				if len(data) > size {
+					write = data[:size]
+				}
+			}
+			n, err := out.Write(write)
+			data = data[n:]
+			if err != nil {
+				t.Fatal(err)
+			}
+		}
+	}(data[:])
+
+	dataout, err := ioutil.ReadAll(in)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !bytes.Equal(dataout, data) {
+		t.Fatal("got bad data")
+	}
+}
+
+func BenchmarkAll(t *testing.B) {
 	var data [bufsize]byte
+
+	rand.Seed(0xDEADBEEF)
 
 	if _, err := rand.Read(data[:]); err != nil {
 		t.Fatal(err)
 	}
 
-	t.ResetTimer()
+	pipeFns := map[string]pipeFunc{
+		"io":  ioPipe,
+		"tcp": tcpPipe,
+	}
+
+	cmFns := map[string]chopperMaker{
+		"alternating-large":     alternatingLargeChopper,
+		"alternating-medium":    alternatingMediumChopper,
+		"alternating-small":     alternatingSmallChopper,
+		"weighted_random-large": randomLargeChopper,
+		"weighted_random-small": randomSmallChopper,
+	}
+
+	bufFns := map[string]writeBufferFunc{
+		"bufio": writeBufferBufio,
+		"dbuf":  writeBufferDBuf,
+		"none":  writeBufferNone,
+	}
+	for bn, bufFn := range bufFns {
+		for pn, pipeFn := range pipeFns {
+			for cn, cmFn := range cmFns {
+				name := fmt.Sprintf("buffer=%s;pipe=%s;chunking=%s", bn, pn, cn)
+				t.Run(name, func(t *testing.B) {
+					runBenchmark(t, data[:], pipeFn, cmFn, bufFn)
+				})
+			}
+		}
+	}
+}
+
+func runBenchmark(t *testing.B, data []byte, pipeFn pipeFunc, cmFn chopperMaker, bufFn writeBufferFunc) {
 	t.StopTimer()
+	t.ResetTimer()
+
+	t.SetBytes(int64(len(data)))
 
 	for i := 0; i < t.N; i++ {
-		in, out := pipe(t)
+		in, out := pipeFn(t)
+		bufout := bufFn(out)
+		chopper := cmFn()
+		rand.Seed(1912)
 		t.StartTimer()
-		bufout := &BufWriteCloser{bw: *bufio.NewWriter(out), inner: out}
-		doTest(t, data[:], bufout, in)
+		doTest(t, data, chopper, bufout, in)
 		t.StopTimer()
 	}
 }
